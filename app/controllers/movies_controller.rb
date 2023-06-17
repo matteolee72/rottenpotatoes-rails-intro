b17d86bdaf611adaab8bcf5526a8b87b42b205fb
Matthias Lee Dong En En <1005898@mymail.sutd.edu.sg>
2023-06-17 13:22:14 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show_hash = params[:ratings]&.keys || {}
+    @movies = Movie.with_ratings(@ratings_to_show_hash)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ['G','PG','PG-13','R']
+  end
+
+  def self.with_ratings(ratings_list)
+    if ratings_list.nil? || ratings_list.empty?
+      all
+    else
+      where(rating: ratings_list.map(&:upcase))
+    end
+  end
 end
